63177011107bd1f74dd3e2972c3d4fa31513fd54
Marwa Mostafa <marwa.mostafa420@gmail.com>
2022-05-18 21:08:07 +0200

    marwa

--- Admin/admin.py
+++ Admin/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Admin, Patient, Doctor, Department, ReservationTicket
+# from .models import Admin, Patient, Doctor, Department, ReservationTicket
 
 
-admin.site.register((Admin, Patient, Doctor, Department, ReservationTicket,))
+# admin.site.register((Admin, Patient, Doctor, Department, ReservationTicket,))
+
+
+from .models import Admin , Patient,Doctor,Department,ReservationTicket
+
+admin.site.register((Admin , Patient,Doctor,Department,ReservationTicket))
--- Admin/models.py
+++ Admin/models.py
@@ -1,51 +1,82 @@
 from datetime import datetime
+from operator import mod
 from socket import inet_aton
-from django.db import models
+# from django.db import models
 from datetime import datetime
+from django.db import models
 # Create your models here.
 
 
 class Admin(models.Model):
-    ID = models.IntegerField()
-    Name = models.CharField()
-    Email = models.CharField()
-    Password = models.CharField()
+    ID = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=100)
+    Email = models.EmailField()
+    Password = models.CharField(max_length=50)
 
-
-class Patient(models.Model):
-    ID = models.IntegerField()
-    Name = models.CharField()
-    BirthDay = models.DateTimeField()
-    WorkName = models.CharField()
-    City = models.CharField()
-    Phone = models.IntegerField(max_length=11)
 
 
 class Doctor(models.Model):
-    ID = models.IntegerField()
-    Name = models.CharField()
-    BirthDay = models.DateTimeField(initial=datetime.now())
-    WorkName = models.CharField()
-    City = models.CharField()
-    Phone = models.IntegerField(max_length=11)
-    Special = models.CharField()
+    ID = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=100)
+    WorkName = models.CharField(max_length=100)
+    City = models.CharField(max_length=100)
+    Phone = models.CharField(max_length=11)
+    Special = models.CharField(max_length=100)
+
+    see=models.ForeignKey(Admin,on_delete=models.PROTECT)
+
+
+
+class Patient(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=100)
+    BirthDay = models.DateTimeField()
+    WorkName = models.CharField(max_length=100)
+    City = models.CharField(max_length=100)
+    Phone = models.CharField(max_length=11)
+    reserved =models.BooleanField(default=False)
+
+    see=models.ForeignKey(Admin,on_delete=models.PROTECT)
+    examin=models.ForeignKey(Doctor,on_delete=models.PROTECT)
+    
+    
+
+
+
+
 
 
 class Department(models.Model):
-    Code = models.IntegerField()
-    Name = models.CharField()
+    Code = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=100)
 
     # Multi value
-    Appiontment = models.DateTimeField()
+    # Appiontment = models.DateTimeField()
+    choose=models.OneToOneField(Patient,on_delete=models.PROTECT,null=False)
+    work=models.OneToOneField(Doctor,on_delete=models.PROTECT)
+
+
+
+
+class Appointment(models.Model):
+    code=models.IntegerField(primary_key=True)
+    dep_Id=models.IntegerField()
+    day=models.CharField(max_length=10)
+
+    time=models.ForeignKey(Department,on_delete=models.CASCADE)
+
 
 
 class ReservationTicket(models.Model):
-    Code = models.IntegerField()
-    PatientName = models.CharField()
-    PatientDoctor = models.CharField()
-    Department = models.CharField()
-    Disease = models.CharField()
+    Code = models.IntegerField(primary_key=True)
+    PatientName = models.CharField(max_length=100)
+    PatientDoctor = models.CharField(max_length=100)
+    Department = models.CharField(max_length=100)
+    Disease = models.CharField(max_length=100)
 
     # 0 ---> على حسابك
     # 1--> على نفقه الدوله
-    State_of_treatement = models.BooleanField(initial=0)
+    State_of_treatement = models.BooleanField(default=False)
+
+    write=models.OneToOneField(Doctor,on_delete=models.PROTECT)
+    have=models.OneToOneField(Patient,on_delete=models.PROTECT)
